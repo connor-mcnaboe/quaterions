4dce57834b4233763d4efeff3b47e8aba7b3b127
Connor McNaboe <mcnaboeconnor@gmail.com>
2018-12-24 10:52:25 -0500

    ADDED: Added quaterions operations

--- src/quaterion.py
+++ src/quaterion.py
@@ -2,6 +2,9 @@
 # Author: Connor McNaboe
 # Purpose: Transform reference frames using quaterions 
 
+# Quaterions in the form q = [q1, q2, q3, q4] where q1-3 are the vector part
+# q4 is the scalar part. 
+
 from math import * 
 import numpy as np
 from numpy.linalg import inv
@@ -42,7 +45,6 @@ def quterion_axis(e, PHI):
 	q3 = e[2]*sin(PHI/2)
 	q4 = cos(PHI/2)
 	q = np.array([q1, q2, q3])
-	print(q)
 	check = q1**2 + q2**2 + q3**2 + q4**2
 	print("Check axis: " + str(check) + "\n")
 	return q, q4
@@ -66,47 +68,63 @@ def rot_axis(q, q4):
 def transform(q, r):
 	r_in = np.append([0], r)
 	
-	qmtrx = np.array([[(q[0]**2 + q[1]**2+ q[2]**2 + q[3]**2), 0, 0, 0], \
-		[0, (q[0]**2 + q[1]**2- q[2]**2 - q[3]**2), (2*q[1]*q[2] - 2*q[0]*q[3]), \
-		(2*q[1]*q[3]+2*q[0]*q[2])], [0, (2*q[1]*q[2] + 2*q[0]*q[3]), \
-		(q[0]**2 - q[1]**2+ q[2]**2 - q[3]**2), (2*q[2]*q[3] - 2*q[0]*q[1])], \
-		[0, (2*q[1]*q[3]-2*q[0]*q[2]), (2*q[2]*q[3] + 2*q[0]*q[1]), \
-		(q[0]**2 -q[1]**2- q[2]**2 + q[3]**2)]])
-	print(qmtrx)
+	qmtrx = np.array([[(q[3]**2 + q[0]**2+ q[1]**2 + q[2]**2), 0, 0, 0], \
+		[0, (q[3]**2 + q[0]**2- q[1]**2 - q[2]**2), (2*q[0]*q[1] - 2*q[3]*q[2]), \
+		(2*q[0]*q[2]+2*q[3]*q[1])], [0, (2*q[0]*q[1] + 2*q[3]*q[2]), \
+		(q[3]**2 - q[0]**2+ q[1]**2 - q[2]**2), (2*q[1]*q[2] - 2*q[3]*q[0])], \
+		[0, (2*q[0]*q[2]-2*q[3]*q[1]), (2*q[1]*q[2] + 2*q[3]*q[0]), \
+		(q[3]**2 -q[0]**2- q[1]**2 + q[2]**2)]])
 	r_out = r_in.dot(qmtrx)
 	return r_out
 
-def transfrom2(q, r):
-	r_in = np.append([0], r)
-	qcon = np.array([q[0], -1*q[1], -1*q[2], -1*q[3]])
-	r_out = q*r_in*qcon
-	print(r_out)
+
+def quat_conj(q):
+	# Function takes a quaterion and retruns its conjugate...numpy only does
+	# This for imaginary vectors (unless i am stupid and dunno how to do it)
+	qcon = np.array([-1*q[0], -1*q[1], -1*q[2], q[3]])
+	return qcon 
+
+def vect_to_quat(r, s):
+	# takes 3D vector and converts it to a quaterion
+	rq = np.append(r, s)
+	return rq
 
 
 def quat_cross(q1, q2):
 	# q2xq1, quaterion operation for "cross" product
-	r1 = q1[3]*q2[0:2] + q2[3]*q1[0:2] - vect_cross(q2[0:2], q1[0:2])
-	r2 = q2[3]*q1[3] - q2[0:2].dot(q1[0:2])
-	prod = np.array([r1], [r2])
+	r1 = np.sum([q1[3]*q2[0:3], q2[3]*q1[0:3], -vect_cross(q2[0:3], q1[0:3]).T])
+	r2 = q2[3]*q1[3] -q2[0:3].dot(q1[0:3])
+	prod = np.append(r1, r2)
+	print(r1)
+	#print(r2)
+	return prod
 
 def vect_cross(x, y): 
 	# Need to define function for vecotr cross product becuase apprently 
 	# Numpy can only do matticies? Stupid! (or maybe i am)
-
 	XxY = np.array([[x[1]*y[2] - x[2]*y[1]], [x[2]*y[0] - x[0]*y[2]], \
 		[x[0]*y[1] - x[1]*y[0]]])
+	return XxY
 
-# Rotation 1
-PHI1 = radians(-90)	     # Principle angle 
+# Rotation 1 # 
+
+# Establish Euler parameters
+PHI1 = radians(180)	     # Principle angle 
 e1 = np.array([0,1,0])   # Euler axis
-
-qax1, q4ax1 = quterion_axis(e1, PHI1)
-Cax = rot_axis(qax1, q4ax1)
-qax = np.append(q4ax1, qax1)
 r = np.array([1,0,0])
+
+# Determine quaterions from euler parameters
+qax1, q4ax1 = quterion_axis(e1, PHI1)
+
+# Rotation from quaterion -> rotation matrix 
+Cax = rot_axis(qax1, q4ax1)
 r_ax1 = Cax.dot(r)
-r_ax2 = transform(qax, r)
-transfrom2(qax, r)
+
+# Rotation from quaterion operations
+qax = np.append(q4ax1, qax1)
+rq = vect_to_quat(r, 0)
+qcon = quat_conj(qax)
+r_ax2 = quat_cross(qax, quat_cross(rq, qcon))
 print("Vectors: \n")
 print(r)
 print(r_ax1)
@@ -118,7 +136,7 @@ ax = plt.axes(projection='3d')
 
 ax.scatter(r[0], r[1], r[2], zdir='z', c='r')
 ax.scatter(r_ax1[0], r_ax1[1], r_ax1[2], zdir='z',  c='b')
-ax.scatter(r_ax2[1], r_ax2[2], r_ax2[3], zdir='z',  c='g')
+ax.scatter(r_ax2[0], r_ax2[1], r_ax2[2], zdir='z',  c='g')
 
 for a  in r:
     rx = np.linspace(0, r[0], 100)
